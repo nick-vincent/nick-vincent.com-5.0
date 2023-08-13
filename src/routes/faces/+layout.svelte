e2db9f5ffffbde6f74004a6cd376da2573051c6b
Nick Vincent <the.nick.vincent@gmail.com>
2023-08-12 19:25:10 -0700

    Scroll to image after navigating to it

--- src/routes/faces/+layout.svelte
+++ src/routes/faces/+layout.svelte
@@ -1,15 +1,23 @@
 <script>
+	import { afterNavigate } from '$app/navigation';
 	import PageTransition from '$lib/PageTransition.svelte';
 
 	export let data;
 
+	let anchor;
+
 	$: pathParts = data.pathname.split('/');
 	$: slug = pathParts[2];
+
+	afterNavigate(() => {
+		if (slug) anchor.scrollIntoView();
+	});
 </script>
 
 <div class="intro">
 	<h1>Faces</h1>
 	<p>Portraits of various famous folks.</p>
+	<div bind:this={anchor}></div>
 </div>
 
 <PageTransition key={slug}>
--- src/routes/photos/+layout.svelte
+++ src/routes/photos/+layout.svelte
@@ -1,15 +1,23 @@
 <script>
+	import { afterNavigate } from '$app/navigation';
 	import PageTransition from '$lib/PageTransition.svelte';
 
 	export let data;
 
+	let anchor;
+
 	$: pathParts = data.pathname.split('/');
 	$: slug = pathParts[2];
+
+	afterNavigate(() => {
+		if (slug) anchor.scrollIntoView();
+	});
 </script>
 
 <div class="intro">
 	<h1>Photos</h1>
 	<p>Photography from around the world.</p>
+	<div bind:this={anchor}></div>
 </div>
 
 <PageTransition key={slug}>
