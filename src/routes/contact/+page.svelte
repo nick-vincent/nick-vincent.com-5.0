8bcec812d8c7ae550a5b2218e7b9c94d1b355f43
Nick Vincent <the.nick.vincent@gmail.com>
2023-08-13 17:52:05 -0700

    Add LinkedIn icon & update contact page

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,12 +1,22 @@
+<script>
+	import LinkedInLink from '$lib/ui/LinkedInLink.svelte';
+	import InstagramLink from '$lib/ui/InstagramLink.svelte';
+</script>
+
 <svelte:head>
 	<title>Nick Vincent → Contact</title>
-	<meta
-		name="description"
-		content="the.nick.vincent@gmail.com"
-	/>
+	<meta name="description" content="Connect or drop me a line." />
 </svelte:head>
 
 <div class="intro">
 	<h1>Contact</h1>
-	<p><a href="mailto:the.nick.vincent@gmail.com">the.nick.vincent@gmail.com</a></p>
+	<p>Connect or drop me a line.</p>
 </div>
+
+<ul class="inline">
+	<li><a href="mailto:the.nick.vincent@gmail.com">Email</a></li>
+	<li><LinkedInLink url="https://www.linkedin.com/in/nickvincent/" /></li>
+	<li><InstagramLink url="https://www.instagram.com/the.nick.vincent/" /></li>
+</ul>
+
+<p></p>
