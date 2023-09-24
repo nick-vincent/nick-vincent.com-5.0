c938534cecb629770026a18f514e4b4dd57324fa
Nick Vincent <the.nick.vincent@gmail.com>
2023-09-24 01:19:44 -0700

    Footer tweaks

--- src/routes/footer.svelte
+++ src/routes/footer.svelte
@@ -7,31 +7,28 @@
 		<li><a href="/photos/">Photos</a></li>
 		<li><a href="/contact/">Contact</a></li>
 	</ul>
-	<p>
-		Built with <a href="https://svelte.dev" rel="external noreferrer" target="_blank">Svelte</a>
-	</p>
 </footer>
 
 <style>
 	footer {
-		padding: 2rem 0 3rem;
-		text-align: center;
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+		gap: 2rem;
+		overflow: scroll;
+		padding: 0.5rem 0 3rem;
+		text-transform: uppercase;
+		font-size: 0.75rem;
+		letter-spacing: 0.1em;
+		font-size: 0.75rem;
+		font-weight: 500;
 	}
 
-	@media (min-width: 860px) {
-		ul {
-			display: flex;
-			align-items: center;
-			justify-content: space-between;
-			gap: 1rem;
-		}
-
-		footer {
-			display: flex;
-			align-items: center;
-			justify-content: space-between;
-			gap: 2rem;
-			padding: 0.25rem 0 3rem;
-		}
+	ul {
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+		flex-wrap: nowrap;
+		gap: 1rem;
 	}
 </style>
